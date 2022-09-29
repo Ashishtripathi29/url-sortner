fccd1d7160b9d777f29ff5ad3b7cc04d55844a8e
Ashishtripathi29 <ashishtripathipara@gmail.com>
2022-09-29 20:21:23 +0530

    done

---
+++ src/routes/urlModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+const usrModel = new mongoose.Schema(
+    {
+        urlCode: {
+            type:String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+
+        longUrl: {
+            type:String,
+            required: true
+        },
+        shortUrl: {
+            type:String,
+            required: true,
+            unique: true
+        }
+
+    }
+)
+
+module.exports = mongoose.model("url", usrModel)
